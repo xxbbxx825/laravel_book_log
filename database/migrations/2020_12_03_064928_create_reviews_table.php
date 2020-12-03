21bfdaa51ef1ea8e7fe67a9895a6b0640039572c
github xxbbxx825 <xxbbxx825@gmail.com>
2020-12-03 16:22:56 +0900

    reviewsテーブルを追加

---
+++ database/migrations/2020_12_03_064928_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 255);
+            $table->string('author', 100);
+            $table->string('status', 10);
+            $table->integer('score')->length(1);
+            $table->string('summary', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
